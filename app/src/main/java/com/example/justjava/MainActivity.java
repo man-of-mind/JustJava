4803146ae7650dc84314fda1de8e8f4c1143ba9a
timothy <timothyabiodun300@gmail.com>
2020-07-28 13:41:58 +0100

    practice

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         TextView quantity = findViewById(R.id.quantity_value);
         int quan = Integer.parseInt(quantity.getText().toString());
         int value = calculatePrice(quan);
-        displayMessage(orderSummary(value));
+        orderSummary(value);
 
     }
 
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return value * val;
     }
-    private String orderSummary(int val){
+    private void orderSummary(int val){
         TextView t = findViewById(R.id.quantity_value);
         String order = t.getText().toString();
         String message = "Name: " + nameOfUser();
@@ -48,7 +51,13 @@ public class MainActivity extends AppCompatActivity {
         message += "\nAdd chocolate? " + chocolateState();
         message += "\nQuantity: " + order;
         message += "\n" +  "Total price: $" + val + "\nThank you!";
-        return message;
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "ORDER SUMMARY");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 
     private String nameOfUser() {
@@ -71,22 +80,30 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.quantity_value);
         textView.setText("" + number);
     }
-    private void displayPrice(int number){
-        TextView price = (TextView) findViewById(R.id.order_summary_text);
-        price.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
     public void increment(View view){
         TextView textView = (TextView) findViewById(R.id.quantity_value);
-        int value = Integer.parseInt(textView.getText().toString()) + 1;
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value == 100){
+            Toast toast = Toast.makeText(this,"You can not order more than 100 cup of coffee",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            value += 1;
+        }
         display(value);
     }
     public void decrement(View view){
         TextView textView = (TextView) findViewById(R.id.quantity_value);
-        int value = Integer.parseInt(textView.getText().toString()) - 1;
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value == 1){
+            Toast toast = Toast.makeText(this,"You can not order less than 1 cup of coffee",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            value -= 1;
+        }
         display(value);
-    }
-    private void displayMessage(String message){
-        TextView priceTag = (TextView) findViewById(R.id.order_summary_text);
-        priceTag.setText(message);
     }
 }
